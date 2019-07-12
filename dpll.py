f5ae392d8b02f33dd1f9d74aa4e56c2ce92d1233
SalvatoreZagaria <s.zagaria9@gmail.com>
2019-07-12 15:40:17 +0100

    SZ

--- dpll.py
+++ dpll.py
@@ -1,102 +1,26 @@
 from z3 import *
-'''
-The formula has to be written as a list of lists.
-Allowed operators: AND, OR, NOT, IMPLIES, BI-IMPLIES
-'''
-
-formula_list = ["AND", "x = y", ["OR", ["AND", "y = z", ["NOT", "x = z"]], "x = z"]]
-
-
-def get_operator(operator):
-    if operator == "NOT":
-        return Not
-    elif operator == "AND":
-        return And
-    elif operator == "OR":
-        return Or
-    elif operator == "IMPLIES":
-        return Implies
-    elif operator == "BI-IMPLIES":
-        return operator
-    else:
-        return False
-
-
-def get_inner_most(f_list, to_avoid=None):  # Todo: this works
-    last_one = None
-    while True:
-        if to_avoid == f_list:
-            return last_one
-        if all(type(i) == str for i in f_list):
-            return f_list
-        for i in f_list:
-            if type(i) == list:
-                last_one = f_list
-                f_list = i
-                break
-
-
-def get_sat_formula(sat_formula, operator_str, variables):
-    if sat_formula is None:
-        if operator_str == "BI-IMPLIES":
-            return variables[0] == variables[1]
-        elif operator_str == "NOT":
-            return Not(variables[0])
-        else:
-            operator = get_operator(operator_str)
-            if operator:
-                return operator(variables)
-            else:
-                print("{}: operator not valid.".format(operator_str))
-                return False
-    else:
-        if operator_str == "BI-IMPLIES":
-            return variables[0] == sat_formula
-        elif operator_str == "NOT":
-            return Not(sat_formula)
-        else:
-            operator = get_operator(operator_str)
-            if operator:
-                variables.append(sat_formula)
-                return operator(variables)
-            else:
-                print("{}: operator not valid.".format(operator_str))
-                return False
-
-
-def sat_solver(f_list):
-    sat_formula = None
-    inner_most = get_inner_most(f_list)
-    while True:
-        variables = []
-        if inner_most is None:
-            break
-        else:
-            for i in inner_most[1:]:
-                if type(i) == str:
-                    variables.append(Bool(i))
-            sat_formula = get_sat_formula(sat_formula, inner_most[0], variables)
-        inner_most = get_inner_most(f_list, to_avoid=inner_most)
-
-    return sat_formula
-
-
-if __name__ == "__main__":
-    sat_formula = sat_solver(formula_list)
-    s = Solver()
-    s.add(sat_formula)
-    s.check()
-    m = s.model()
-    variables_for_dtp = []
-    for d in m.decls():
-        if m[d]:
-            variables_for_dtp.append(d)
-        else:
-            variables_for_dtp.append(Not(d))
-    dtp = And(variables_for_dtp)
-    solve(dtp)
-
-
+# import builtins
+# import os
 #
-# dpt = And(x == y, y == z, Not(x == z))
-# solve(dpt)
+# builtins.Z3_LIB_DIRS = [os.path.join("C:", "Users", "vulzaa", "Desktop", "Z3", "venv", "Lib", "site-packages", "z3",
+#                                      "lib", "libz3.dll")]
+
+c1 = 1, 3, -4
+c2 = -1, 2
+c3 = -1, -4, 3
+c4 = -1, 3, 5
+
+x1 = Bool("x1")
+x2 = Bool("x2")
+x3 = Bool("x3")
+x4 = Bool("x4")
+x5 = Bool("x5")
+
+s = Solver()
+s.add(Or(x1, x3, Not(x4)))
+s.add(Or(Not(x1), x2))
+s.add(Or(Not(x1), Not(x4), x3))
+s.add(Or(Not(x1), x3, x5))
+
+print(s.check())
+print(s.model())
---
+++ dpll_wrong.py
@@ -0,0 +1,102 @@
+from z3 import *
+'''
+The formula has to be written as a list of lists.
+Allowed operators: AND, OR, NOT, IMPLIES, BI-IMPLIES
+'''
+
+formula_list = ["AND", "x = y", ["OR", ["AND", "y = z", ["NOT", "x = z"]], "x = z"]]
+
+
+def get_operator(operator):
+    if operator == "NOT":
+        return Not
+    elif operator == "AND":
+        return And
+    elif operator == "OR":
+        return Or
+    elif operator == "IMPLIES":
+        return Implies
+    elif operator == "BI-IMPLIES":
+        return operator
+    else:
+        return False
+
+
+def get_inner_most(f_list, to_avoid=None):
+    last_one = None
+    while True:
+        if to_avoid == f_list:
+            return last_one
+        if all(type(i) == str for i in f_list):
+            return f_list
+        for i in f_list:
+            if type(i) == list:
+                last_one = f_list
+                f_list = i
+                break
+
+
+def get_sat_formula(sat_formula, operator_str, variables):
+    if sat_formula is None:
+        if operator_str == "BI-IMPLIES":
+            return variables[0] == variables[1]
+        elif operator_str == "NOT":
+            return Not(variables[0])
+        else:
+            operator = get_operator(operator_str)
+            if operator:
+                return operator(variables)
+            else:
+                print("{}: operator not valid.".format(operator_str))
+                return False
+    else:
+        if operator_str == "BI-IMPLIES":
+            return variables[0] == sat_formula
+        elif operator_str == "NOT":
+            return Not(sat_formula)
+        else:
+            operator = get_operator(operator_str)
+            if operator:
+                variables.append(sat_formula)
+                return operator(variables)
+            else:
+                print("{}: operator not valid.".format(operator_str))
+                return False
+
+
+def sat_solver(f_list):
+    sat_formula = None
+    inner_most = get_inner_most(f_list)
+    while True:
+        variables = []
+        if inner_most is None:
+            break
+        else:
+            for i in inner_most[1:]:
+                if type(i) == str:
+                    variables.append(Bool(i))
+            sat_formula = get_sat_formula(sat_formula, inner_most[0], variables)
+        inner_most = get_inner_most(f_list, to_avoid=inner_most)
+
+    return sat_formula
+
+
+if __name__ == "__main__":
+    sat_formula = sat_solver(formula_list)
+    s = Solver()
+    s.add(sat_formula)
+    s.check()
+    m = s.model()
+    variables_for_dtp = []
+    for d in m.decls():
+        if m[d]:
+            variables_for_dtp.append(d)
+        else:
+            variables_for_dtp.append(Not(d))
+    dtp = And(variables_for_dtp)
+    solve(dtp)
+
+
+#
+# dpt = And(x == y, y == z, Not(x == z))
+# solve(dpt)
